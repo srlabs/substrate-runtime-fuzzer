c7c5f770e85dd7926ddb5971dc1a0d2477713f92
Louis Merlin <louis@srlabs.de>
2025-11-25 13:54:27 +0100

    Add call-codec fuzzer

---
+++ call-codec/src/main.rs
@@ -0,0 +1,20 @@
+use codec::{DecodeLimit, Encode};
+use kitchensink_runtime::RuntimeCall;
+
+fn main() {
+    ziggy::fuzz!(|data: &[u8]| {
+        let mut extrinsic_data = data;
+        // TODO Try to remove the depth limit
+        let maybe_extrinsic: Result<RuntimeCall, codec::Error> =
+            DecodeLimit::decode_with_depth_limit(64, &mut extrinsic_data);
+        if let Ok(extrinsic) = maybe_extrinsic {
+            #[cfg(not(feature = "fuzzing"))]
+            println!("Found extrinsic: {extrinsic}");
+            let original_extrinsic_data = &data[..data.len() - extrinsic_data.len()];
+            let re_encoding = Encode::encode(&extrinsic);
+            assert_eq!(original_extrinsic_data, re_encoding);
+            #[cfg(not(feature = "fuzzing"))]
+            println!("Original encoding matches re-encoding");
+        }
+    });
+}
