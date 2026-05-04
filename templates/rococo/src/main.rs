bcc1bb98c553668b9df7c5ff0f08f2f3136cd7c8
Louis Merlin <louis@srlabs.de>
2026-05-04 15:56:14 +0200

    Add rococo fuzzer

---
+++ templates/rococo/src/main.rs
@@ -0,0 +1,273 @@
+#![warn(clippy::pedantic)]
+use codec::{DecodeLimit, Encode};
+use frame_support::{
+    dispatch::GetDispatchInfo,
+    pallet_prelude::Weight,
+    traits::{IntegrityTest, TryState, TryStateSelect},
+    weights::constants::WEIGHT_REF_TIME_PER_SECOND,
+};
+use frame_system::Account;
+use pallet_balances::{Holds, TotalIssuance};
+use polkadot_primitives::{AccountId, Balance, Header, InherentData};
+use rococo_runtime::{
+    AllPalletsWithSystem, Balances, Executive, ParaInherent, Runtime, RuntimeCall, RuntimeOrigin,
+    Timestamp,
+};
+use sp_consensus_babe::{
+    digests::{PreDigest, SecondaryPlainPreDigest},
+    Slot, BABE_ENGINE_ID,
+};
+use sp_runtime::{
+    testing::H256,
+    traits::{Header as _, Dispatchable},
+    Digest, DigestItem, Storage,
+};
+use sp_state_machine::BasicExternalities;
+use std::{
+    iter,
+    time::{Duration, Instant},
+};
+
+const SLOT_DURATION: u64 = 6000;
+
+fn main() {
+    sp_tracing::try_init_simple();
+    let accounts: Vec<AccountId> = (0..5).map(|i| [i; 32].into()).collect();
+    let genesis = generate_genesis(&accounts);
+
+    ziggy::fuzz!(|data: &[u8]| {
+        process_input(&accounts, &genesis, data);
+    });
+}
+
+fn generate_genesis(accounts: &[AccountId]) -> Storage {
+    use rococo_runtime::{
+        BalancesConfig, GrandpaConfig, RuntimeGenesisConfig, SudoConfig, SystemConfig,
+        TransactionPaymentConfig, SessionConfig, SessionKeys
+    };
+    use sp_runtime::BuildStorage;
+    use sp_application_crypto::{Pair, ByteArray};
+    use pallet_grandpa::AuthorityId as GrandpaId;
+    use sp_authority_discovery::AuthorityId as AuthorityDiscoveryId;
+    use sp_consensus_babe::AuthorityId as BabeId;
+    use polkadot_primitives::{AssignmentId, ValidatorId};
+
+    // Configure endowed accounts with initial balance of 1 << 60.
+    let balances = accounts.iter().cloned().map(|k| (k, 1 << 60)).collect();
+
+    RuntimeGenesisConfig {
+        system: SystemConfig::default(),
+        balances: BalancesConfig {
+            balances,
+            dev_accounts: None,
+        },
+        assigned_slots: Default::default(),
+        beefy: Default::default(),
+        claims: Default::default(),
+        babe: Default::default(),
+        configuration: Default::default(),
+        hrmp: Default::default(),
+        nis_counterpart_balances: Default::default(),
+        paras: Default::default(),
+        registrar: Default::default(),
+        society: Default::default(),
+        vesting: Default::default(),
+        xcm_pallet: Default::default(),
+        indices: Default::default(),
+        session: SessionConfig {
+            keys: vec![(
+                [0; 32].into(),
+                [0; 32].into(),
+                SessionKeys {
+                    grandpa: GrandpaId::from_slice(&[0; 32]).unwrap(),
+                    babe: BabeId::from_slice(&[0; 32]).unwrap(),
+                    beefy: sp_consensus_beefy::ecdsa_crypto::Pair::from_string("", None).unwrap().public(),
+                    authority_discovery: AuthorityDiscoveryId::from_slice(&[0; 32]).unwrap(),
+                    para_assignment: AssignmentId::from_slice(&[0; 32]).unwrap(),
+                    para_validator: ValidatorId::from_slice(&[0; 32]).unwrap(),
+                },
+            )],
+            non_authority_keys: vec![],
+        },
+        authority_discovery: Default::default(),
+        treasury: Default::default(),
+        grandpa: GrandpaConfig::default(),
+        sudo: SudoConfig { key: None }, // Assign no network admin rights.
+        transaction_payment: TransactionPaymentConfig::default(),
+    }
+    .build_storage()
+    .unwrap()
+}
+
+fn process_input(accounts: &[AccountId], genesis: &Storage, data: &[u8]) {
+    // We build the list of extrinsics we will execute
+    let mut data = data;
+    // Vec<(advance_block, origin, extrinsic)>
+    let extrinsics: Vec<(bool, u8, RuntimeCall)> =
+        iter::from_fn(|| DecodeLimit::decode_with_depth_limit(64, &mut data).ok())
+            .filter(|(_, _, x)| {
+                matches!(
+                    x,
+                    RuntimeCall::System(_)
+                        | RuntimeCall::Utility(_)
+                        | RuntimeCall::Proxy(_)
+                        | RuntimeCall::Balances(_)
+                        | RuntimeCall::Vesting(_)
+                        | RuntimeCall::Multisig(_)
+                )
+            })
+            .collect();
+    if extrinsics.is_empty() {
+        return;
+    }
+
+    let mut block: u32 = 1;
+    let mut weight: Weight = 0.into();
+    let mut elapsed: Duration = Duration::ZERO;
+
+    BasicExternalities::execute_with_storage(&mut genesis.clone(), || {
+        let initial_total_issuance = TotalIssuance::<Runtime>::get();
+
+        initialize_block(block);
+
+        for (advance_block, origin, extrinsic) in extrinsics {
+            if advance_block {
+                finalize_block(elapsed);
+
+                block += 1;
+                weight = 0.into();
+                elapsed = Duration::ZERO;
+
+                initialize_block(block);
+            }
+
+            let pre_weight = extrinsic.get_dispatch_info().call_weight;
+            let cumulative_weight = weight.saturating_add(pre_weight);
+            if cumulative_weight.ref_time() >= 2 * WEIGHT_REF_TIME_PER_SECOND {
+                #[cfg(not(feature = "fuzzing"))]
+                println!("Extrinsic would exhaust block weight, skipping");
+                continue;
+            }
+            weight = cumulative_weight;
+
+            let origin = accounts[origin as usize % accounts.len()].clone();
+
+            #[cfg(not(feature = "fuzzing"))]
+            println!("\n    origin:     {origin:?}");
+            #[cfg(not(feature = "fuzzing"))]
+            println!("    call:       {extrinsic:?}");
+
+            let now = Instant::now(); // We get the current time for timing purposes.
+            let res = extrinsic.dispatch(RuntimeOrigin::signed(origin));
+            elapsed += now.elapsed();
+
+            #[cfg(not(feature = "fuzzing"))]
+            println!("    result:     {res:?}");
+
+            let actual_weight = res.unwrap_or_else(|e| e.post_info).actual_weight;
+            let post_weight = actual_weight.unwrap_or_default();
+            assert!(pre_weight.ref_time() >= post_weight.ref_time(), "Pre-dispatch weight ref time ({}) is smaller than post-dispatch weight ref time ({})", pre_weight.ref_time(), post_weight.ref_time());
+            assert!(pre_weight.proof_size() >= post_weight.proof_size(), "Pre-dispatch weight proof size ({}) is smaller than post-dispatch weight proof size ({})", pre_weight.proof_size(), post_weight.proof_size());
+        }
+
+        finalize_block(elapsed);
+
+        check_invariants(block, initial_total_issuance);
+    });
+}
+
+fn initialize_block(block: u32) {
+    #[cfg(not(feature = "fuzzing"))]
+    println!("\ninitializing block {block}");
+
+    let pre_digest = Digest {
+        logs: vec![DigestItem::PreRuntime(
+            BABE_ENGINE_ID,
+            PreDigest::SecondaryPlain(SecondaryPlainPreDigest {
+                slot: Slot::from(u64::from(block)),
+                authority_index: 42,
+            })
+            .encode(),
+        )],
+    };
+
+    let grandparent_header: Header = Header::new(
+        block,
+        H256::default(),
+        H256::default(),
+        <frame_system::Pallet<Runtime>>::parent_hash(),
+        pre_digest.clone(),
+    );
+
+    Executive::initialize_block(&Header::new(
+        block,
+        H256::default(),
+        H256::default(),
+        grandparent_header.hash(),
+        pre_digest,
+    ));
+
+    #[cfg(not(feature = "fuzzing"))]
+    println!("  setting timestamp");
+    Timestamp::set(RuntimeOrigin::none(), u64::from(block) * SLOT_DURATION).unwrap();
+
+    #[cfg(not(feature = "fuzzing"))]
+    println!("  setting bitfields");
+    ParaInherent::enter(
+        RuntimeOrigin::none(),
+        InherentData {
+            parent_header: grandparent_header,
+            backed_candidates: Vec::default(),
+            bitfields: Vec::default(),
+            disputes: Vec::default(),
+        },
+    )
+    .unwrap();
+}
+
+fn finalize_block(elapsed: Duration) {
+    #[cfg(not(feature = "fuzzing"))]
+    println!("\n  time spent: {elapsed:?}");
+    assert!(elapsed.as_secs() <= 2, "block execution took too much time");
+
+    #[cfg(not(feature = "fuzzing"))]
+    println!("  finalizing block");
+    Executive::finalize_block();
+}
+
+fn check_invariants(block: u32, initial_total_issuance: Balance) {
+    let mut counted_free = 0;
+    let mut counted_reserved = 0;
+    for (account, info) in Account::<Runtime>::iter() {
+        let consumers = info.consumers;
+        let providers = info.providers;
+        assert!(!(consumers > 0 && providers == 0), "Invalid c/p state");
+        counted_free += info.data.free;
+        counted_reserved += info.data.reserved;
+        let max_lock: Balance = Balances::locks(&account)
+            .iter()
+            .map(|l| l.amount)
+            .max()
+            .unwrap_or_default();
+        assert_eq!(
+            max_lock, info.data.frozen,
+            "Max lock should be equal to frozen balance"
+        );
+        let sum_holds: Balance = Holds::<Runtime>::get(&account)
+            .iter()
+            .map(|l| l.amount)
+            .sum();
+        assert!(
+            sum_holds <= info.data.reserved,
+            "Sum of all holds ({sum_holds}) should be less than or equal to reserved balance {}",
+            info.data.reserved
+        );
+    }
+    let total_issuance = TotalIssuance::<Runtime>::get();
+    let counted_issuance = counted_free + counted_reserved;
+    assert_eq!(total_issuance, counted_issuance);
+    assert!(total_issuance <= initial_total_issuance);
+    // We run all developer-defined integrity tests
+    AllPalletsWithSystem::integrity_test();
+    AllPalletsWithSystem::try_state(block, TryStateSelect::All).unwrap();
+}
