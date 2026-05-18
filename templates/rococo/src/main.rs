9418bb939700879add16e843e355970df92d87dc
Louis Merlin <louis@srlabs.de>
2026-05-18 11:41:24 +0200

    Remove sp-state-machine and add FuzzingExternalities to remaining harnesses

--- templates/rococo/src/main.rs
+++ templates/rococo/src/main.rs
@@ -17,12 +17,12 @@ use sp_consensus_babe::{
     digests::{PreDigest, SecondaryPlainPreDigest},
     Slot, BABE_ENGINE_ID,
 };
+use sp_fuzzing::FuzzingExternalities;
 use sp_runtime::{
     testing::H256,
     traits::{Dispatchable, Header as _},
     Digest, DigestItem, Storage,
 };
-use sp_state_machine::BasicExternalities;
 use std::{
     iter,
     time::{Duration, Instant},
@@ -148,7 +148,7 @@ fn process_input(accounts: &[AccountId], genesis: &Storage, data: &[u8]) {
     let mut weight: Weight = 0.into();
     let mut elapsed: Duration = Duration::ZERO;
 
-    BasicExternalities::execute_with_storage(&mut genesis.clone(), || {
+    FuzzingExternalities::execute_with_storage(&mut genesis.clone(), || {
         let initial_total_issuance = TotalIssuance::<Runtime>::get();
 
         initialize_block(block);
